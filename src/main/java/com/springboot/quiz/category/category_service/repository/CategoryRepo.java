7edff7a928f2a3168c4c90c44f76e9cc8d318176
bipinstha07 <shresthab2@warhawks.ulm.edu>
2025-07-19 17:50:49 -0500

    update

---
+++ src/main/java/com/springboot/quiz/category/category_service/repository/CategoryRepo.java
@@ -0,0 +1,7 @@
+package com.springboot.quiz.category.category_service.repository;
+
+import com.springboot.quiz.category.category_service.entity.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CategoryRepo extends JpaRepository<Category,Long> {
+}
